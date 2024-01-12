86428a690ceff90b38c58da3d82dfc51096a5e63
Aram Bayadyan <aram.bv@gmail.com>
2024-01-12 19:55:04 +0400

    added expiry date and row coloring, fixed rendering issues

--- pages/warranties.tsx
+++ pages/warranties.tsx
@@ -66,28 +66,47 @@
 //   }
 // }
 
-import React, { useState } from "react";
-import clientPromise from "../lib/mongodb";
-import DataTable from "react-data-table-component";
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+import clientPromise from '../lib/mongodb';
+import DataTable from 'react-data-table-component';
 
 export default function Products({ products }) {
   const [items, setItems] = useState(products);
+  // Next.js has a bug where it tries to render the component before the window object is available.
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return <></>;
+  // End of bug fix
 
   const columns = [
     {
-      name: "Product Name",
+      name: 'Product Name',
       selector: (row) => row.Name,
+      // sortable: true,
+    },
+    {
+      name: 'SKU',
+      selector: (row) => row.SKU,
+    },
+    {
+      name: 'Expiry Date',
+      selector: (row) => moment(new Date(Number(row.expiryDate))).format('DD/MM/YYYY HH:mm:ss'),
       sortable: true,
     },
     {
-      name: "SKU",
-      selector: (row) => row.SKU,
+      name: 'Expired',
+      selector: (row) => (row.expiryDate < Date.now() ? 'Yes' : 'No'),
+      sortable: true,
     },
   ];
 
   const paginationComponentOptions = {
-    rowsPerPageText: "Rows per page",
-    rangeSeparatorText: "of",
+    rowsPerPageText: 'Rows per page',
+    rangeSeparatorText: 'of',
     selectAllRowsItem: true,
   };
 
@@ -97,6 +116,19 @@ export default function Products({ products }) {
     });
     setItems(filteredItems);
   };
+
+  const conditionalRowStyles = [
+    {
+      when: (row) => row.expiryDate < Date.now(),
+      style: {
+        backgroundColor: 'rgba(242, 38, 19, 0.9)',
+        color: 'white',
+        '&:hover': {
+          cursor: 'not-allowed',
+        },
+      },
+    },
+  ];
 
   return (
     <div>
@@ -109,8 +141,9 @@ export default function Products({ products }) {
           columns={columns}
           data={items}
           pagination
-          paginationComponentOptions={paginationComponentOptions}
-        ></DataTable>
+          responsive
+          conditionalRowStyles={conditionalRowStyles}
+          paginationComponentOptions={paginationComponentOptions}></DataTable>
       </div>
     </div>
   );
@@ -119,13 +152,9 @@ export default function Products({ products }) {
 export async function getServerSideProps() {
   try {
     const client = await clientPromise;
-    const db = client.db("yatoam");
+    const db = client.db('yatoam');
 
-    const products = await db
-      .collection("products")
-      .find({})
-      .limit(5000)
-      .toArray();
+    const products = await db.collection('warranties').find({}).limit(5000).toArray();
 
     return {
       props: { products: JSON.parse(JSON.stringify(products)) },
